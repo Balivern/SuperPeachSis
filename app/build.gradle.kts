b046441dd15ff4f1c3e461cd4e3f90f862702e95
BAH Saliou Bailo <salioubailobah33@gmail.com>
2026-03-20 09:10:53 +0100

    fix : Update project configuration and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.superpeachsis"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.superpeachsis"
