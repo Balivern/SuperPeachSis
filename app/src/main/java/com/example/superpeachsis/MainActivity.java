66377b2cd2bd97bc568ead55bff3e3a5a6a1bcab
Balivern <renancabane53@gmail.com>
2026-03-20 08:48:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/superpeachsis/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.superpeachsis;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        GameView gameView = new GameView(this);
+        setContentView(gameView);
+
+        ViewCompat.setOnApplyWindowInsetsListener(gameView, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        int valeur_y = sharedPref.getInt("valeur_y", 0);
+        valeur_y = (valeur_y + 100) % 400;
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("valeur_y", valeur_y);
+        editor.apply();
+    }
+}
