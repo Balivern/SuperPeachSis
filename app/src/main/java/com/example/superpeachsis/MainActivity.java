d45550541cdc28c74579ad1009155e893eb75122
Renan Cabane <123549102+Balivern@users.noreply.github.com>
2026-03-20 11:12:30 +0100

    Revert "Sps assets init (#5)"
    This reverts commit ebae5b93d5f21e28edaf1e58c9b4da6f5ccf5f73.

--- app/src/main/java/com/example/superpeachsis/MainActivity.java
+++ app/src/main/java/com/example/superpeachsis/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
